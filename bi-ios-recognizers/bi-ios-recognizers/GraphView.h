39ebd43a318a96f1b1157ff9eba5c534c9ccc4c0
Dominik Vesely <vesely@dominoo.cz>
2014-10-07 10:03:38 +0200

    graph

---
+++ bi-ios-recognizers/bi-ios-recognizers/GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  bi-ios-recognizers
+//
+//  Created by Dominik Vesely on 07/10/14.
+//  Copyright (c) 2014 Ackee s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
