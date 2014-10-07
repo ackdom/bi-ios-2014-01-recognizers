67b069dec84c8cf88270ef99d42500c2f374ae85
Dominik Vesely <vesely@dominoo.cz>
2014-10-07 09:26:45 +0200

    chartview

---
+++ bi-ios-recognizers/bi-ios-recognizers/GraphView.m
@@ -0,0 +1,21 @@
+//
+//  GraphView.m
+//  bi-ios-recognizers
+//
+//  Created by Dominik Vesely on 07/10/14.
+//  Copyright (c) 2014 Ackee s.r.o. All rights reserved.
+//
+
+#import "GraphView.h"
+
+@implementation GraphView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
