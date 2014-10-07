57c73a0cc86bb809330e8e87afdd87d6d6160972
Dominik Vesely <vesely@dominoo.cz>
2014-10-07 09:34:11 +0200

    initial for slave

--- bi-ios-recognizers/bi-ios-recognizers/ViewController.m
+++ bi-ios-recognizers/bi-ios-recognizers/ViewController.m
@@ -17,8 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView* v = [[UIview alloc] initWithFrame:CGRectMake(10, 20, 100, 100)];
+    v.backgroundColor = [UIColor redColor];
+    
+    UITapGestureRecognizer* tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(oneTap:)];
+    
+    [self.view addSubview:v];
+    
+    
 }
 
+- (void) oneTap:(UITapGestureRecognizer*) {
+    NSLog(@"OneTap");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
